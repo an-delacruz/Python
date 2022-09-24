5877e181821cf2b0382c7c10dbc9faf54b1d8446
Antonio De la cruz <antoniocruzrivera0216@gmail.com>
2022-09-24 18:55:28 -0500

    Cambios ejercicios 2 y 3

--- Unidad 1/Clases/Rectangulo.py
+++ Unidad 1/Clases/Rectangulo.py
@@ -1,27 +1,27 @@
-#definicion de una clase en python (hereda de object)
-class Rectangulo:
-    def __init__(self,ancho, largo) -> None:
-        self._ancho=ancho
-        self._largo = largo
-    @property 
-    def Ancho(self):
-        return self._ancho
-    @Ancho.setter #hacen referencia al metodo de la propiedad
-    def Ancho(self,a):
-        self._ancho=a
-    #se llama para agregar datos al objeto.
-    def CalcularArea(self):
-        return (self._ancho*self._largo)
-    def CalcularPerimetro(self):
-        return (2*(self._ancho+self._largo)
+# #definicion de una clase en python (hereda de object)
+# class Rectangulo:
+#     def __init__(self,ancho, largo) -> None:
+#         self._ancho=ancho
+#         self._largo = largo
+#     @property 
+#     def Ancho(self):
+#         return self._ancho
+#     @Ancho.setter #hacen referencia al metodo de la propiedad
+#     def Ancho(self,a):
+#         self._ancho=a
+#     #se llama para agregar datos al objeto.
+#     def CalcularArea(self):
+#         return (self._ancho*self._largo)
+#     def CalcularPerimetro(self):
+#         return (2*(self._ancho+self._largo)
     
- class Cuadrado:
-    def __init__(self,lado) -> None:
-        self._lado=lado
-    def Area(self):
-        return self._lado  * self._lado
-    def Perimetro(self):
-        return self._lado*4
+#  class Cuadrado:
+#     def __init__(self,lado) -> None:
+#         self._lado=lado
+#     def Area(self):
+#         return self._lado  * self._lado
+#     def Perimetro(self):
+#         return self._lado*4
     
     
     
