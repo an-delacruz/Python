0942aef006834bf7e2b3d17e5f73bee35236d57b
Antonio De la cruz <antoniocruzrivera0216@gmail.com>
2022-10-12 09:48:08 -0500

    App Personas, modelos persona y domicilio

--- Unidad 2/Django/mainapp/mainapp/settings.py
+++ Unidad 2/Django/mainapp/mainapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp',
+    'personas'
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,17 @@ WSGI_APPLICATION = 'mainapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Django-1',
+        'USER': 'postgres',
+        'PASSWORD':'admin',
+        'HOST': 'localhost',
+        'PORT':'5432'
     }
 }
 
--- Unidad 2/Django/mainapp/webapp/models.py
+++ Unidad 2/Django/mainapp/webapp/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-from mainapp.webapp import admin
-
 # Create your models here.
-admin.site.register()
