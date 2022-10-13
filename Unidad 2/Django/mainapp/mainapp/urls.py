69a5c8ed82cd9847fe7e329176839e5782d139f1
Antonio De la cruz <antoniocruzrivera0216@gmail.com>
2022-10-13 09:31:29 -0500

    Bienvenido view

--- Unidad 2/Django/mainapp/mainapp/urls.py
+++ Unidad 2/Django/mainapp/mainapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import bienvenido
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',bienvenido)
 ]
--- Unidad 2/Django/mainapp/webapp/views.py
+++ Unidad 2/Django/mainapp/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from personas.models import Persona
+def bienvenido(request):
+    personas = Persona.objects.order_by('id')
+    return render(request,'bienvenido.html',{'personas':personas})
